72a60fb087fb428d7eb4cdcf9353dbf85a500e90
elijah <cuiyeqing960904@gmail.com>
2023-08-27 15:56:08 -0400

    kafka connected

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
 func main() {
-	
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id": "test-client",
+		"acks": "all",
+	})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+
+	delivery_chan := make(chan kafka.Event, 10000)
+	topic := "test-topic"
+
+	err = p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value: []byte("test123")},
+		delivery_chan,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := <-delivery_chan
+	fmt.Printf("%+v\n", e.String())
+
+	fmt.Printf("%+v\n", p)
 }
