596247b8f6a382e444d8aa928d59350ca9fd17f2
elijah <cuiyeqing960904@gmail.com>
2023-08-27 16:31:42 -0400

    go producer + consumer

--- main.go
+++ main.go
@@ -4,37 +4,66 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	topic := "topic"
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
-		"client.id": "test-client",
-		"acks": "all",
+		"client.id":         "test-client",
+		"acks":              "all",
 	})
 
 	if err != nil {
 		fmt.Printf("Failed to create producer: %s\n", err)
-		os.Exit(1)
 	}
+
+	go func() {
+		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers": "localhost:9092",
+			"group.id":          "test-client",
+			"auto.offset.reset": "smallest"},
+		)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = consumer.Subscribe(topic, nil)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			ev := consumer.Poll(100)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("consumed: %s\n", string(e.Value))
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+			default:
+				fmt.Printf("Ignored %v\n", e)
+			}
+		}
+	}()
 
 	delivery_chan := make(chan kafka.Event, 10000)
-	topic := "test-topic"
+	for {
+		err = p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte("FOO")},
+			delivery_chan,
+		)
 
-	err = p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-		Value: []byte("test123")},
-		delivery_chan,
-	)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	if err != nil {
-		log.Fatal(err)
+		<-delivery_chan
+		time.Sleep(time.Second * 2)
 	}
-
-	e := <-delivery_chan
-	fmt.Printf("%+v\n", e.String())
-
-	fmt.Printf("%+v\n", p)
 }
